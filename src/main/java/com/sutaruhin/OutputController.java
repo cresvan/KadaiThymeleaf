9c7ccf17a916ee86368aef4c33f883388506c6c7
louis <louis@DESKTOP-2VE7A13>
2023-06-16 22:29:42 +0800

    Thymeleafの課題を作成

---
+++ src/main/java/com/sutaruhin/OutputController.java
@@ -0,0 +1,19 @@
+package com.sutaruhin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutputController {
+
+	@PostMapping("/output")
+	public String postOutput(@RequestParam("val") String val, Model model) {
+
+		model.addAttribute("outputmsg", "メッセージ: " + val);
+
+		return "output";
+	}
+
+}
